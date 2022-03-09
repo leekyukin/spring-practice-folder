f1d4686fb4f34868bee7a65e1dcd83e01e96d00e
leekyukin <leekujin14@gmail.com>
2022-03-09 23:41:32 +0900

    2022-03-09 Setting for review

---
+++ spring-security-review1/src/main/java/com/security/springsecurityreview1/SpringSecurityReview1Application.java
@@ -0,0 +1,13 @@
+package com.security.springsecurityreview1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityReview1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityReview1Application.class, args);
+    }
+
+}
---
+++ spring-security-review1/src/test/java/com/security/springsecurityreview1/SpringSecurityReview1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.springsecurityreview1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityReview1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
