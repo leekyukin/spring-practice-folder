f1805ddfafda9b12cc9cfcdde3d5f62f75ac0f0d
leekyukin <leekujin14@gmail.com>
2022-03-12 20:29:39 +0900

    2022-03-12 Ready to start

---
+++ spring-review2/src/main/java/com/example/springreview2/SpringReview2Application.java
@@ -0,0 +1,13 @@
+package com.example.springreview2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReview2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReview2Application.class, args);
+    }
+
+}
---
+++ spring-review2/src/test/java/com/example/springreview2/SpringReview2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springreview2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReview2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
