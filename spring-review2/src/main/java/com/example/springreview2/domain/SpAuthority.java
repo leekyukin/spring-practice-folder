18df2ecec7e98d0c47e63ce17205b37d36b93823
leekyukin <leekujin14@gmail.com>
2022-03-14 09:12:15 +0900

    2022-03-14 Impelement entities

---
+++ spring-review2/src/main/java/com/example/springreview2/domain/SpAuthority.java
@@ -0,0 +1,27 @@
+package com.example.springreview2.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "sp_user_authority")
+@IdClass(SpAuthority.class)
+public class SpAuthority implements GrantedAuthority {
+
+    @Id
+    @Column(name = "user_id")
+    private Long userid;
+
+    @Id
+    private String Authority;
+
+}
